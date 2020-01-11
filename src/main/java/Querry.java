80e89071c77586f2ca34f2c00cfa662ad0fffe50
Michał Koperski <mkoperpno@gmali.com>
2020-01-11 22:15:45 +0100

    Add new method in class Querry: getBookToList

--- src/main/java/Querry.java
+++ src/main/java/Querry.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Querry {
 
@@ -8,32 +10,40 @@ public class Querry {
 
 
     public void getUsers() {
-    try{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/ksiegarnia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-        String user = "sdatest";
-        String password = "Start123!";
-        connection = DriverManager.getConnection(url, user, password);
-        statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM uzytkownik");
-        while (resultSet.next())
-        {
-            Integer id = resultSet.getInt("id");
-            String imie = resultSet.getString("imie");
-            String nazwisko = resultSet.getString("nazwisko");
-            System.out.println(id+" "+imie+" "+nazwisko);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/ksiegarnia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            String user = "sdatest";
+            String password = "Start123!";
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM uzytkownik");
+            while (resultSet.next()) {
+                Integer id = resultSet.getInt("id");
+                String imie = resultSet.getString("imie");
+                String nazwisko = resultSet.getString("nazwisko");
+                System.out.println(id + " " + imie + " " + nazwisko);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-    }catch (ClassNotFoundException e)
-    {
-        e.printStackTrace();
-    }catch (SQLException e)
-    {
-        e.printStackTrace();
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-}
 
     public void getBooks() {
-        try{
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/ksiegarnia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
             String user = "sdatest";
@@ -41,19 +51,16 @@ public class Querry {
             connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM ksiazka");
-            while (resultSet.next())
-            {
+            while (resultSet.next()) {
                 Integer id = resultSet.getInt("id");
                 String tytul = resultSet.getString("tytul");
-                System.out.println(id+" "+tytul);
+                System.out.println(id + " " + tytul);
             }
-        }catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        }catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 statement.close();
             } catch (SQLException e) {
@@ -67,33 +74,30 @@ public class Querry {
         }
     }
 
-    public void getUsersByName(String name)
-    {
+    public void getUsersByName(String name) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/ksiegarnia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
             String user = "sdatest";
             String password = "Start123!";
-            connection=DriverManager.getConnection(url,user,password);
-            preparedStatement=connection.prepareStatement("SELECT * FROM uzytkownik WHERE imie =\""+name+"\"");
+            connection = DriverManager.getConnection(url, user, password);
+            preparedStatement = connection.prepareStatement("SELECT * FROM uzytkownik WHERE imie =\"" + name + "\"");
             boolean isResultSet = preparedStatement.execute();
-            if(isResultSet) {
+            if (isResultSet) {
                 ResultSet resultSet = preparedStatement.getResultSet();
                 while (resultSet.next()) {
-                    Integer id =resultSet.getInt("id");
+                    Integer id = resultSet.getInt("id");
                     String imie = resultSet.getString("imie");
                     String nazwisko = resultSet.getString("nazwisko");
-                    System.out.println(id+" "+imie+" "+nazwisko);
+                    System.out.println(id + " " + imie + " " + nazwisko);
                 }
             }
 
-        }catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        }catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 preparedStatement.close();
             } catch (SQLException e) {
@@ -107,25 +111,22 @@ public class Querry {
         }
     }
 
-    public void updateNameAndSurnameById(Integer id, String name, String surname)
-    {
+    public void updateNameAndSurnameById(Integer id, String name, String surname) {
         //UPDATE uzytkownik SET imie = "Michal", nazwisko = "Kulpa" WHERE id=1;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/ksiegarnia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
             String user = "sdatest";
             String password = "Start123!";
-            connection=DriverManager.getConnection(url,user,password);
-            preparedStatement=connection.prepareStatement("UPDATE uzytkownik SET imie = \""+name+"\",nazwisko=\""+surname+"\" WHERE id="+id);
+            connection = DriverManager.getConnection(url, user, password);
+            preparedStatement = connection.prepareStatement("UPDATE uzytkownik SET imie = \"" + name + "\",nazwisko=\"" + surname + "\" WHERE id=" + id);
             preparedStatement.executeUpdate();
 
-        }catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        }catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 preparedStatement.close();
             } catch (SQLException e) {
@@ -137,9 +138,45 @@ public class Querry {
                 e.printStackTrace();
             }
         }
-
-
     }
 
 
+    public List<Book> getBookToList() {
+        ArrayList<Book> books = new ArrayList<>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/ksiegarnia?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            String user = "sdatest";
+            String password = "Start123!";
+            Connection connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM ksiazka");
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setId(resultSet.getInt("id"));
+                book.setTytul(resultSet.getString("tytul"));
+                books.add(book);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+//        try {
+//            connection.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+        return books;
+    }
+
 }
+
+
+
